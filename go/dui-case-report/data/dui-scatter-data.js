4df1d28a6e62b0bad6e495be52ea639005e71d7e
Jeff Lotter <jeff@jlotterlaw.com>
2026-06-06 14:13:57 -0400

    MC #592 wire DUI scatter aggregate data

---
+++ go/dui-case-report/data/dui-scatter-data.js
@@ -0,0 +1,702 @@
+window.duiScatterDataPayload = {
+  "metadata": {
+    "agencyCount": 8,
+    "grain": "agency/officer/anonymized filing-period bucket",
+    "minBucketCases": 3,
+    "minOfficerCases": 6,
+    "officerCount": 33,
+    "period": "recent court-filing period buckets",
+    "rowCount": 86,
+    "source": "court_filings current_state parquet joined to dim_agency.display_name and dim_officer display fields"
+  },
+  "rows": [
+    {
+      "agency": "Apopka Police Department",
+      "bac": 0.072,
+      "caseCount": 4,
+      "month": 2,
+      "officer": "Daniel J. Shipley",
+      "under08Count": 2
+    },
+    {
+      "agency": "Apopka Police Department",
+      "bac": 0.149,
+      "caseCount": 4,
+      "month": 11,
+      "officer": "Juan Sanchez",
+      "under08Count": 0
+    },
+    {
+      "agency": "Belle Isle Police Department",
+      "bac": 0.04,
+      "caseCount": 4,
+      "month": 12,
+      "officer": "Zachary Mathews",
+      "under08Count": 2
+    },
+    {
+      "agency": "Maitland Police Department",
+      "bac": 0.115,
+      "caseCount": 3,
+      "month": 9,
+      "officer": "B Rosa",
+      "under08Count": 0
+    },
+    {
+      "agency": "Maitland Police Department",
+      "bac": 0.109,
+      "caseCount": 3,
+      "month": 3,
+      "officer": "Gary Andrews",
+      "under08Count": 0
+    },
+    {
+      "agency": "Maitland Police Department",
+      "bac": 0.123,
+      "caseCount": 3,
+      "month": 7,
+      "officer": "Gary Andrews",
+      "under08Count": 1
+    },
+    {
+      "agency": "Maitland Police Department",
+      "bac": 0.115,
+      "caseCount": 4,
+      "month": 12,
+      "officer": "Gary Andrews",
+      "under08Count": 1
+    },
+    {
+      "agency": "Maitland Police Department",
+      "bac": 0.064,
+      "caseCount": 4,
+      "month": 1,
+      "officer": "Payton L. Moreno",
+      "under08Count": 2
+    },
+    {
+      "agency": "Maitland Police Department",
+      "bac": 0.175,
+      "caseCount": 3,
+      "month": 6,
+      "officer": "Payton L. Moreno",
+      "under08Count": 0
+    },
+    {
+      "agency": "Maitland Police Department",
+      "bac": 0.156,
+      "caseCount": 3,
+      "month": 7,
+      "officer": "Payton L. Moreno",
+      "under08Count": 0
+    },
+    {
+      "agency": "Maitland Police Department",
+      "bac": 0.244,
+      "caseCount": 3,
+      "month": 8,
+      "officer": "Payton L. Moreno",
+      "under08Count": 0
+    },
+    {
+      "agency": "Ocoee Police Department",
+      "bac": 0.207,
+      "caseCount": 5,
+      "month": 6,
+      "officer": "Bryan Valdes",
+      "under08Count": 0
+    },
+    {
+      "agency": "Ocoee Police Department",
+      "bac": 0.172,
+      "caseCount": 3,
+      "month": 6,
+      "officer": "Luis Silva",
+      "under08Count": 0
+    },
+    {
+      "agency": "Ocoee Police Department",
+      "bac": 0.108,
+      "caseCount": 4,
+      "month": 7,
+      "officer": "Luis Silva",
+      "under08Count": 1
+    },
+    {
+      "agency": "Ocoee Police Department",
+      "bac": 0.0,
+      "caseCount": 4,
+      "month": 8,
+      "officer": "Luis Silva",
+      "under08Count": 3
+    },
+    {
+      "agency": "Ocoee Police Department",
+      "bac": 0.149,
+      "caseCount": 6,
+      "month": 10,
+      "officer": "Luis Silva",
+      "under08Count": 1
+    },
+    {
+      "agency": "Ocoee Police Department",
+      "bac": 0.191,
+      "caseCount": 4,
+      "month": 11,
+      "officer": "Luis Silva",
+      "under08Count": 1
+    },
+    {
+      "agency": "Ocoee Police Department",
+      "bac": 0.12,
+      "caseCount": 3,
+      "month": 12,
+      "officer": "Luis Silva",
+      "under08Count": 1
+    },
+    {
+      "agency": "Ocoee Police Department",
+      "bac": 0.225,
+      "caseCount": 3,
+      "month": 13,
+      "officer": "Luis Silva",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.13,
+      "caseCount": 4,
+      "month": 5,
+      "officer": "Ajay Bhatia",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.236,
+      "caseCount": 3,
+      "month": 13,
+      "officer": "Anthony Acosta",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.152,
+      "caseCount": 3,
+      "month": 11,
+      "officer": "Benjamin Thomas",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.101,
+      "caseCount": 3,
+      "month": 1,
+      "officer": "Diarmuid McGrory",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.159,
+      "caseCount": 3,
+      "month": 4,
+      "officer": "Diarmuid McGrory",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.195,
+      "caseCount": 3,
+      "month": 6,
+      "officer": "Galloza M. Alicea",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.203,
+      "caseCount": 3,
+      "month": 7,
+      "officer": "Galloza M. Alicea",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.16,
+      "caseCount": 6,
+      "month": 8,
+      "officer": "Galloza M. Alicea",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.135,
+      "caseCount": 3,
+      "month": 10,
+      "officer": "Galloza M. Alicea",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.171,
+      "caseCount": 5,
+      "month": 11,
+      "officer": "Galloza M. Alicea",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.104,
+      "caseCount": 4,
+      "month": 1,
+      "officer": "John Spencer",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.174,
+      "caseCount": 3,
+      "month": 3,
+      "officer": "Paul Ortiz",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.097,
+      "caseCount": 7,
+      "month": 1,
+      "officer": "Richard Sheiman",
+      "under08Count": 3
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.11,
+      "caseCount": 9,
+      "month": 2,
+      "officer": "Richard Sheiman",
+      "under08Count": 3
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.089,
+      "caseCount": 3,
+      "month": 4,
+      "officer": "Richard Sheiman",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.0,
+      "caseCount": 5,
+      "month": 6,
+      "officer": "Richard Sheiman",
+      "under08Count": 3
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.03,
+      "caseCount": 7,
+      "month": 7,
+      "officer": "Richard Sheiman",
+      "under08Count": 5
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.0,
+      "caseCount": 3,
+      "month": 8,
+      "officer": "Richard Sheiman",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.073,
+      "caseCount": 4,
+      "month": 9,
+      "officer": "Richard Sheiman",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.0,
+      "caseCount": 3,
+      "month": 11,
+      "officer": "Richard Sheiman",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.0,
+      "caseCount": 3,
+      "month": 13,
+      "officer": "Richard Sheiman",
+      "under08Count": 3
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.109,
+      "caseCount": 3,
+      "month": 5,
+      "officer": "Rosado G. Marrero",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.0,
+      "caseCount": 3,
+      "month": 6,
+      "officer": "Rosado G. Marrero",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.234,
+      "caseCount": 3,
+      "month": 7,
+      "officer": "Rosado G. Marrero",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.276,
+      "caseCount": 3,
+      "month": 8,
+      "officer": "Rosado G. Marrero",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.178,
+      "caseCount": 3,
+      "month": 10,
+      "officer": "Rosado G. Marrero",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.223,
+      "caseCount": 3,
+      "month": 12,
+      "officer": "Rosado G. Marrero",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.0,
+      "caseCount": 3,
+      "month": 13,
+      "officer": "Rosado G. Marrero",
+      "under08Count": 3
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.209,
+      "caseCount": 3,
+      "month": 10,
+      "officer": "Zachary Jefremow",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orange County Sheriff's Office",
+      "bac": 0.019,
+      "caseCount": 9,
+      "month": 11,
+      "officer": "Zachary Jefremow",
+      "under08Count": 5
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.181,
+      "caseCount": 3,
+      "month": 6,
+      "officer": "Adem Altinerlielmas",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.13,
+      "caseCount": 4,
+      "month": 11,
+      "officer": "Adem Altinerlielmas",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.16,
+      "caseCount": 4,
+      "month": 9,
+      "officer": "Andres Ruiz",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.141,
+      "caseCount": 3,
+      "month": 4,
+      "officer": "Aracely Cantu",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.214,
+      "caseCount": 3,
+      "month": 7,
+      "officer": "Ariel Clabaugh",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.178,
+      "caseCount": 4,
+      "month": 1,
+      "officer": "Damon Daniels",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.17,
+      "caseCount": 3,
+      "month": 3,
+      "officer": "Damon Daniels",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.295,
+      "caseCount": 3,
+      "month": 6,
+      "officer": "Delince Jules",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.136,
+      "caseCount": 4,
+      "month": 7,
+      "officer": "Delince Jules",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.0,
+      "caseCount": 3,
+      "month": 8,
+      "officer": "Delince Jules",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.0,
+      "caseCount": 3,
+      "month": 9,
+      "officer": "Delince Jules",
+      "under08Count": 3
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.154,
+      "caseCount": 3,
+      "month": 10,
+      "officer": "Delince Jules",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.122,
+      "caseCount": 13,
+      "month": 11,
+      "officer": "Delince Jules",
+      "under08Count": 3
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.165,
+      "caseCount": 8,
+      "month": 12,
+      "officer": "Delince Jules",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.064,
+      "caseCount": 8,
+      "month": 13,
+      "officer": "Delince Jules",
+      "under08Count": 4
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.086,
+      "caseCount": 5,
+      "month": 1,
+      "officer": "Edwin Rosario",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.055,
+      "caseCount": 4,
+      "month": 2,
+      "officer": "Edwin Rosario",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.111,
+      "caseCount": 4,
+      "month": 6,
+      "officer": "Joseph Vazquez",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.137,
+      "caseCount": 7,
+      "month": 7,
+      "officer": "Joseph Vazquez",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.0,
+      "caseCount": 3,
+      "month": 8,
+      "officer": "Joseph Vazquez",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.057,
+      "caseCount": 4,
+      "month": 9,
+      "officer": "Joseph Vazquez",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.086,
+      "caseCount": 4,
+      "month": 10,
+      "officer": "Joseph Vazquez",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.102,
+      "caseCount": 6,
+      "month": 11,
+      "officer": "Joseph Vazquez",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.156,
+      "caseCount": 6,
+      "month": 12,
+      "officer": "Joseph Vazquez",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.03,
+      "caseCount": 6,
+      "month": 13,
+      "officer": "Joseph Vazquez",
+      "under08Count": 4
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.187,
+      "caseCount": 3,
+      "month": 4,
+      "officer": "Joshua Rios",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.12,
+      "caseCount": 4,
+      "month": 5,
+      "officer": "Joshua Rios",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.142,
+      "caseCount": 3,
+      "month": 8,
+      "officer": "Joshua Rios",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.085,
+      "caseCount": 3,
+      "month": 9,
+      "officer": "Joshua Rios",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.168,
+      "caseCount": 7,
+      "month": 10,
+      "officer": "Joshua Rios",
+      "under08Count": 1
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.148,
+      "caseCount": 5,
+      "month": 11,
+      "officer": "Joshua Rios",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.192,
+      "caseCount": 3,
+      "month": 4,
+      "officer": "Juan Carvajal",
+      "under08Count": 0
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.094,
+      "caseCount": 4,
+      "month": 4,
+      "officer": "Steven Santos",
+      "under08Count": 2
+    },
+    {
+      "agency": "Orlando Police Department",
+      "bac": 0.079,
+      "caseCount": 5,
+      "month": 8,
+      "officer": "William Lufkin",
+      "under08Count": 3
+    },
+    {
+      "agency": "Winter Garden Police Department",
+      "bac": 0.198,
+      "caseCount": 3,
+      "month": 7,
+      "officer": "Corey T. Conte",
+      "under08Count": 0
+    },
+    {
+      "agency": "Winter Park Police Department",
+      "bac": 0.179,
+      "caseCount": 3,
+      "month": 4,
+      "officer": "A Torres",
+      "under08Count": 1
+    },
+    {
+      "agency": "Winter Park Police Department",
+      "bac": 0.122,
+      "caseCount": 3,
+      "month": 10,
+      "officer": "C Campbell",
+      "under08Count": 1
+    }
+  ]
+};
---
+++ go/dui-case-report/scripts/build-dui-scatter-data.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python3
+"""Build privacy-safe aggregate DUI scatter data for the static page.
+
+The public artifact contains only canonical agency/officer labels, anonymized
+filing-period buckets, aggregate case counts, bucket median BAC, and under-.08
+counts. It intentionally excludes case numbers, defendant names, exact dates,
+raw filing text, and database credentials.
+"""
+
+from __future__ import annotations
+
+import argparse
+import json
+from datetime import date
+from pathlib import Path
+
+import polars as pl
+
+
+DEFAULT_DATA_ROOT = Path.home() / "data-analysis" / "output"
+DEFAULT_OUT = Path(__file__).resolve().parents[1] / "data" / "dui-scatter-data.js"
+MIN_OFFICER_CASES = 6
+MIN_BUCKET_CASES = 3
+
+FORBIDDEN_KEYS = {
+    "case_number",
+    "defendant",
+    "dob",
+    "address",
+    "dl_num",
+    "citation_number",
+    "raw_file_id",
+    "file_date",
+    "offense_date",
+    "arrest_date",
+}
+
+
+def _read_sources(data_root: Path) -> tuple[pl.LazyFrame, pl.LazyFrame, pl.LazyFrame]:
+    current = data_root / "current_state"
+    dims = data_root / "dims"
+    return (
+        pl.scan_parquet(current / "court_filings.parquet"),
+        pl.scan_parquet(dims / "dim_agency.parquet"),
+        pl.scan_parquet(dims / "dim_officer.parquet"),
+    )
+
+
+def _start_month(max_filing_date: date) -> date:
+    return date(max_filing_date.year - 1, max_filing_date.month, 1)
+
+
+def build_rows(data_root: Path) -> tuple[list[dict], dict]:
+    filings, dim_agency, dim_officer = _read_sources(data_root)
+    max_filing_date = (
+        filings
+        .filter((pl.col("is_dui") == True) & pl.col("file_date").is_not_null())
+        .select(pl.col("file_date").max())
+        .collect()
+        .item()
+    )
+    if max_filing_date is None:
+        raise RuntimeError("No DUI filing dates available")
+
+    period_start = _start_month(max_filing_date)
+    agency_dim = dim_agency.select(["agency_key", "display_name"])
+    officer_dim = dim_officer.select(["officer_key", "canonical_name", "name_full_formal"])
+
+    case_level = (
+        filings
+        .filter(
+            (pl.col("is_dui") == True)
+            & (~pl.col("is_future").fill_null(False))
+            & pl.col("file_date").is_not_null()
+            & (pl.col("file_date") >= pl.lit(period_start))
+            & pl.col("primary_agency_key").is_not_null()
+            & pl.col("primary_officer_key").is_not_null()
+            & pl.col("bac_value").is_not_null()
+            & pl.col("bac_value").is_between(0, 0.50)
+            & (pl.col("is_local_agency") == True)
+        )
+        .select([
+            "case_number",
+            "file_date",
+            "primary_agency_key",
+            "primary_officer_key",
+            "primary_agency",
+            "primary_officer",
+            "bac_value",
+        ])
+        .group_by("case_number")
+        .agg([
+            pl.col("file_date").min().alias("file_date"),
+            pl.col("primary_agency_key").drop_nulls().first().alias("agency_key"),
+            pl.col("primary_officer_key").drop_nulls().first().alias("officer_key"),
+            pl.col("primary_agency").drop_nulls().first().alias("primary_agency"),
+            pl.col("primary_officer").drop_nulls().first().alias("primary_officer"),
+            pl.col("bac_value").max().alias("bac"),
+        ])
+        .join(agency_dim, on="agency_key", how="left")
+        .join(officer_dim, on="officer_key", how="left")
+        .with_columns([
+            pl.coalesce([pl.col("display_name"), pl.col("primary_agency")]).alias("agency"),
+            pl.coalesce([
+                pl.col("name_full_formal"),
+                pl.col("canonical_name"),
+                pl.col("primary_officer"),
+            ]).alias("officer"),
+            (
+                ((pl.col("file_date").dt.year() - period_start.year) * 12)
+                + pl.col("file_date").dt.month()
+                - period_start.month
+                + 1
+            ).cast(pl.Int64).alias("month"),
+        ])
+        .filter(pl.col("agency").is_not_null() & pl.col("officer").is_not_null())
+    )
+
+    officer_totals = (
+        case_level
+        .group_by(["agency", "officer"])
+        .agg(pl.len().alias("total_cases"))
+        .filter(pl.col("total_cases") >= MIN_OFFICER_CASES)
+    )
+
+    bucketed = (
+        case_level
+        .join(officer_totals, on=["agency", "officer"], how="inner")
+        .group_by(["agency", "officer", "month"])
+        .agg([
+            pl.len().alias("caseCount"),
+            pl.col("bac").median().round(3).alias("bac"),
+            (pl.col("bac") < 0.08).sum().alias("under08Count"),
+        ])
+        .filter(pl.col("caseCount") >= MIN_BUCKET_CASES)
+        .sort(["agency", "officer", "month"])
+        .collect()
+    )
+
+    rows = [
+        {
+            "agency": row["agency"],
+            "officer": row["officer"],
+            "month": int(row["month"]),
+            "bac": float(row["bac"]),
+            "caseCount": int(row["caseCount"]),
+            "under08Count": int(row["under08Count"]),
+        }
+        for row in bucketed.iter_rows(named=True)
+    ]
+
+    metadata = {
+        "source": "court_filings current_state parquet joined to dim_agency.display_name and dim_officer display fields",
+        "grain": "agency/officer/anonymized filing-period bucket",
+        "period": "recent court-filing period buckets",
+        "minOfficerCases": MIN_OFFICER_CASES,
+        "minBucketCases": MIN_BUCKET_CASES,
+        "rowCount": len(rows),
+        "agencyCount": len({row["agency"] for row in rows}),
+        "officerCount": len({(row["agency"], row["officer"]) for row in rows}),
+    }
+    return rows, metadata
+
+
+def assert_safe_payload(payload: dict) -> None:
+    text = json.dumps(payload, sort_keys=True)
+    for key in FORBIDDEN_KEYS:
+        if key in text:
+            raise RuntimeError(f"Forbidden field leaked into artifact: {key}")
+    for row in payload["rows"]:
+        missing = {"agency", "officer", "month", "bac", "caseCount", "under08Count"} - set(row)
+        if missing:
+            raise RuntimeError(f"Row missing required keys: {sorted(missing)}")
+        if not (1 <= int(row["month"]) <= 13):
+            raise RuntimeError(f"Month bucket out of range: {row['month']}")
+        if int(row["caseCount"]) < MIN_BUCKET_CASES:
+            raise RuntimeError("Bucket support below public threshold")
+
+
+def write_artifact(rows: list[dict], metadata: dict, out_path: Path) -> None:
+    payload = {"metadata": metadata, "rows": rows}
+    assert_safe_payload(payload)
+    out_path.parent.mkdir(parents=True, exist_ok=True)
+    out_path.write_text(
+        "window.duiScatterDataPayload = "
+        + json.dumps(payload, indent=2, sort_keys=True)
+        + ";\n",
+        encoding="utf-8",
+    )
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--data-root", type=Path, default=DEFAULT_DATA_ROOT)
+    parser.add_argument("--out", type=Path, default=DEFAULT_OUT)
+    args = parser.parse_args()
+
+    rows, metadata = build_rows(args.data_root)
+    write_artifact(rows, metadata, args.out)
+    print(json.dumps(metadata, indent=2, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
